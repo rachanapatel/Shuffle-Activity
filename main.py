ce6a4d4a2a90dba3909447dd54832f0729c09c85
rachanapatel <rmp226@scarletmail.rutgers.edu>
2025-03-17 18:25:25 -0400

    updated to return shuffled deck

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ SUITS = ["♥", "♦", "♠", "♣"]
 
 deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 
+
 # Implement a solution for shuffling a deck of cards
 # Use only randint() and no other imports
 def shuffle_deck(deck_of_cards):
-    pass
+    new_deck = []
+    while (len(new_deck) < 52):
+        cards = deck_of_cards[randint(0,len(deck_of_cards) -1)]
+        new_deck.append(cards)
+    print(new_deck)
+
+    return (new_deck)
+    
+shuffle_deck(deck_of_cards)
